9b4149eed66d3c859eaa801682103d712e5ab7d8
Dylan Rowe <dylanthk@gmail.com>
2020-12-17 23:10:37 -0600

    wip

--- dayssinceapi/models/Goals.py
+++ dayssinceapi/models/Goals.py
@@ -1,8 +1,9 @@
+from django.db.models.fields import BooleanField
 from dayssinceapi.models.DaysSinceUser import DaysSinceUser
 from django.db import models
 
 class Goals(models.Model):
     date = models.DateField()
-    goal_name = models.CharField(max_length=200, default='none')
+    goal_name = models.BooleanField(default='false')
     user = models.ForeignKey(DaysSinceUser, on_delete=models.CASCADE)
   
